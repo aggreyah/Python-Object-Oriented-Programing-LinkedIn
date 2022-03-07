4862aa5bd00c2555c5ec7fae787ca13404678e42
aggreyah <aggreyah@gmail.com>
2022-03-07 15:34:22 +0300

    finished the first exercise.

--- Exercise Files/Ch 1/definition_start.py
+++ Exercise Files/Ch 1/definition_start.py
@@ -3,9 +3,14 @@
 
 
 # TODO: create a basic class
-
+class Book:
+    def __init__(self, title):
+        self.title = title
 
 # TODO: create instances of the class
-
+b1 = Book("The demon haunted world.")
+b2 = Book("Cosmos")
 
 # TODO: print the class and property
+print(b1)
+print(b1.title)
